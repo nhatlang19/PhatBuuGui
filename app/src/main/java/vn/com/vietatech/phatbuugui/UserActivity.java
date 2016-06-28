e1e9c40fab14992afd24118f3700d9d63902b88f
Luan Nguyen <nhatlang19@gmail.com>
2016-06-29 00:19:12 +0700

    some changes

--- app/src/main/java/vn/com/vietatech/phatbuugui/UserActivity.java
+++ app/src/main/java/vn/com/vietatech/phatbuugui/UserActivity.java
@@ -1,9 +1,12 @@
 package vn.com.vietatech.phatbuugui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AbsListView;
@@ -78,5 +81,29 @@ public class UserActivity extends AppCompatActivity {
                 Log.e("ListView", "onNothingSelected:");
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.btnAddUser) {
+            Intent intent = new Intent(this, AddUserActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
